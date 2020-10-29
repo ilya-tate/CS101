0c274229bd2cea0ed744a39780435f22369c5c2c
Ilya Tate <ilyatate@gmail.com>
2020-10-29 14:28:08 -0700

    Class 10/29/20

---
+++ Unit 4/Practice/Practice 1/script.js
@@ -0,0 +1,45 @@
+// let firstWord = "Banana";
+// let secondWord = "bandana";
+
+// console.log(`Banana is less than bandana ${firstWord.toLowerCase() < secondWord.toLowerCase()}`);
+
+
+// console.log(`5 > 4: ${5 > 4}`);
+// console.log(`"apple" > "appear": ${"apple" > "appear"}`);
+// console.log(`"west" < "Went": ${"west" < "Went"}`);
+// console.log(`2 > "12": ${2 > "12"}`);
+// console.log(`undefined == null: ${undefined == null}`);
+// console.log(`undefined === null: ${undefined === null}`);
+// console.log(`null == "0": ${null == "0"}`);
+
+
+// if(5 == 5) {
+//     console.log(`It was equal`);
+// }
+
+// if(undefined == null) {
+//     console.log(`They're both nothing`);
+// }
+
+// if(undefined === null) {
+//     console.log(`Something is very wrong`);
+// }
+
+
+// let age = 54;
+// if(age >= 18) console.log('Adult');
+
+
+let check = true;
+
+if(check) console.log(`It was true`);
+if("") console.log(`This is false`); // Will print nothing because false
+
+if("whatever") console.log(`This is true`);
+if(0) console.log(`This is falsy`);
+
+if(check) {
+    console.log(`It was true;`);
+} else {
+    console.log(`It was false`);
+}
