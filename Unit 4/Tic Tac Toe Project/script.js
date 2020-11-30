6500a5077a084ebabe975375162498634c0e5014
Ilya Tate <ilyatate@gmail.com>
2020-11-30 04:58:53 -0700

    Started Tic Tac Toe Project

--- Unit 4/Tic Tac Toe Project/script.js
+++ Unit 4/Tic Tac Toe Project/script.js
@@ -6,26 +6,22 @@
 // Player 2 = O
 
 
-let gameEnd = false;
-let currentPlayer = 1;
+let turn = 1;
+let cells = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']; // letters: columns, rows: numbers
 
-if (amtConnected == 3) {
-    gameEnd = true;
+// Change text in cell when button is clicked
+function insertShape(cell) {
+    // if (turn == 1) {
+    //     turn++;
+    //     document.getElementById(cell).textContent = 'X';
+    // } else {
+    //     turn++;
+    //     document.getElementById(cell).textContent = 'O';
+    // }
+    // console.log(`Turn: ${turn}`);
+    document.getElementById(cell).textContent = 'X';
 }
 
-function nextTurn() {
-    let shapes = ['x', 'o'];
-    let currentShape;
-
-    if (currentPlayer == 1) {
-        currentshape = 'O';
-        currentPlayer++;
-    } else if (currentPlayer == 2) {
-        currentShape = 'X';
-        currentPlayer--;
-    }
-}
-
-function chooseSpot() {
-    
-}
+// function checkGameOver() {
+//     if()
+// }
